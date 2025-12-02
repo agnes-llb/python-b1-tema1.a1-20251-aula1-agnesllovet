b323fa3078f5cb2f9011a8b7b00948c094c413ae
agness <agness.llov@gmail.com>
2025-12-02 20:26:24 +0100

    Realitzat i provat l'exercici 1b3

--- 1b/ej1b3_test.py
+++ 1b/ej1b3_test.py
@@ -12,3 +12,7 @@ def test_line_graph():
     assert ax.get_ylabel() == 'Axis Y', "no label in Axis Y detected"
     assert ax.get_title() == 'Graph', "no title detected"
     assert len(plt.get_fignums()) == 1, "number of figures is not 1"
+
+
+
+test_line_graph()    
